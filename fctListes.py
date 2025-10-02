56212970c4d5ab35128b5d806630e4e7cc9e43cc
Sovannthanant <2472525@cegepoutaouais.qc.ca>
2025-10-02 16:54:51 -0400

    A

--- fctListes.py
+++ fctListes.py
@@ -22,3 +22,71 @@ Nom :                   1   2   3
 Nom :                   2   3   1
 Nom :                   3   1   2
 """
+
+#===========================FONCTION DE LAMARANA===========================
+"""
+1. Fonction qui reçoit les points de vies et les points de défenses d'un joueur et les points d'attaques de l'autre et
+qui retourne les points de vies restants après l'attaque.
+"""
+print("-"*50)
+print("Bienvenue dans la bataille")
+pts_vie=100
+pts_attaque=20
+pts_defense=15
+pts_joueur= pts_vie
+pts_joueur= pts_vie
+
+def points_vies(pts_vie):
+    """
+    fonction qui reçoit des point de vie
+    :return:
+    """
+    return points_vies(pts_vie)
+
+    pass
+def points_attaques(pts_attaque):
+    """fonction de defense
+
+    return:"""
+    return points_attaques(pts_attaque)
+
+def points_defenses(pts_defense):
+    """fonction de defense"""
+    return points_defenses(pts_defense)
+
+def fonction_joueur1():
+    """fonction pour le joueur 1"""
+    return
+def fonction_joueur2():
+    """fonction pour le joueur 2"""
+    pass
+def combat():
+    if fonction_joueur1() > fonction_joueur2():
+        print("Joueur 1 reporte la partie la partie")
+    elif fonction_joueur1() == fonction_joueur2():
+        print("Les joueurs sont a égalité")
+    elif fonction_joueur1() < fonction_joueur2():
+        print("Le Joueur 2 remporte la partie")
+    else:
+        print("le joueur")
+        print("")
+
+print("-"*50)
+
+#----------------------------------------FONCTION DE VANN SOVANNTHANANT----------------------------------------
+
+#def colorer_lego():
+
+#Une liste avec des pièces de légos.
+liste_legos = ["lego","lego2","lego3"]
+couleur = str.lower(input("Qu'elle est la couleur appliquer? "))
+print(liste_legos)
+
+pourcentage = 25
+peinturer = input("Quelle pièce de légos voulez vous peinturez? ")
+if peinturer in liste_legos:
+    print(f"{peinturer} et colorer de {couleur} à {pourcentage}%.")
+else:
+    print("Veuillez choisir une pièce de légo valide.")
+
+print("-"*50)
